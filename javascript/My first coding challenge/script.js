984d7045c67c2690cfdc5adec03b54089a66364f
Hot Cheese <muhammedibrahim613@gmail.com>
2023-11-25 16:37:55 +0500

    Only projects

--- javascript/My first coding challenge/script.js
+++
@@ -1,5 +0,0 @@
-//write code that will return random letter from my name
-
-let myName = "MuhammedIbrahim";
-
-console.log(myName.charAt(Math.floor(Math.random() * myName.length)));
--- javascript/js2/js/script.js
+++
@@ -1,4 +0,0 @@
-console.log("HEllO WORLD");
-console.log(typeof "dave");
-console.log(typeof 42);
-console.log(typeof true);
