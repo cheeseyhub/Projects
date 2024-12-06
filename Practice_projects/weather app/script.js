a07af015a7f33ab160f6887db807c3940bd11cb4
cheeseyhub <126375276+cheeseyhub@users.noreply.github.com>
2024-12-06 05:25:48 +0500

    Update script.js
    removal

--- Practice_projects/weather app/script.js
+++ Practice_projects/weather app/script.js
@@ -1,6 +1,4 @@
-const apiKey = "890795f6d908ab281d764b76893e9c29";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
